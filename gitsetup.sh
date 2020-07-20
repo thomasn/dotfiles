ec4e5bed6c83060b6bdb5e00622dade2348c92b9
Thomas Nichols <thomas@nexus10.com>
2020-07-20 17:25:20 +0100

    Tweaking git setup

---
+++ gitsetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.ls ls-files
+git config --global user.name "Thomas Nichols"
+git config --global user.email "thomas@nexus10.com" 
+
