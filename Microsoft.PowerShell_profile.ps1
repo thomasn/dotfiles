2d87cfb463f0d7d2c056fc22e8cb4fe8f267869d
Thomas Nichols <thomas@nexus10.com>
2022-12-13 02:30:27 +0000

    Tweak uncap config - breaks PoweShell...

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,8 @@
 # Write-Host "Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
 Write-Host "--- $PSCommandPath ---"
-# nv is a PowerShell builtin:
+# nv is a PowerShell builtin
 Remove-Alias -Force -Name nv
 Set-Alias -Name nv -Value nvim
 Set-Alias -Name e -Value nvim
 Set-Alias -Name xe -Value nvim
+Write-Host "Optionally: crash PowerShell with ~\dotfiles\esc-caps-swap.ps1"
