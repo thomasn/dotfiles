f2c73b37e6ccf91f49f78013ac3a1042b3b3c996
Thomas Nichols <thomas@nexus10.com>
2023-05-24 03:18:45 +0100

    Updates after WSL crash

--- linkall.sh
+++ linkall.sh
@@ -5,8 +5,8 @@ mkdir -p ~/.config/nvim
 mkdir -p ~/.cache/dein
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/screenrc ~/.screenrc
-ln -s ~/dotfiles/vimrc ~/.vim/vimrc
-ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
+# ln -s ~/dotfiles/vimrc ~/.vim/vimrc
+# ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
 [[ -f ~/.gitconfig ]] || cp ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/bashrc ~/.bashrc
 [[ "${OSTYPE}" == "netbsd" ]] && mv ~/.bashrc ~/.bash_profile
